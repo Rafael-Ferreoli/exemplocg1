5ee78981fd7f9c8b7fe035bb6c36e2953cd7ce02
Ferreoli <rafaelferreoli@gmail.com>
2021-06-05 21:55:01 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+// aponta para o servidor publico
+app.use(express.static(__dirname + '/public'))
+// aponta para pasta build
+app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
+//
+app.use('/texturas/', express.static(path.join(__dirname, 'texturas')));
+//
+app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
+//
+app.listen(3000, () =>
+    console.log('Visit http://127.0.0.1:3000')
+    );
