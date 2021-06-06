93a18164ca5a58a87682906647c95e94f64a269b
Ferreoli <rafaelferreoli@gmail.com>
2021-06-05 22:11:12 -0300

    adicionando comentarios

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ const path = require('path')
 app.use(express.static(__dirname + '/public'))
 // aponta para pasta build
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')));
-//
+// // aponta para pasta texturas
 app.use('/texturas/', express.static(path.join(__dirname, 'texturas')));
-//
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 //
 app.listen(3000, () =>
